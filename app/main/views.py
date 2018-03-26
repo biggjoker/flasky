196f8db0d2ffddaa10dd8ab8721697ab680715fc
luoxiao <email@example.com>
2018-03-26 18:04:15 +0800

    file add

--- app/main/views.py
+++ app/main/views.py
@@ -9,5 +9,19 @@ def index():
     """主页视图"""
     return render_template("index.html")
 
+@main.route("/about")
+def about():
+    """主页视图"""
+    return render_template("about.html")
+
+@main.route("/photos")
+def photos():
+    """主页视图"""
+    return render_template("photos.html")
+
+@main.route("/contact")
+def contact():
+    """主页视图"""
+    return render_template("contact.html")
 
 
