d98be091566679f043579c327e23b50d16b4e860
luoxiao <email@example.com>
2018-03-26 19:12:56 +0800

    修改页面

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -21,37 +21,7 @@ $(document).ready(function(){
 		function(){ $(this).animate({ top: 0 }, 'fast'); }
 	);
 
-
-	/* -- Using Modernizr to check if the "required" and "placeholder" attributes are supported -- */
-
-	if (!Modernizr.input.placeholder) {
-		$('.email').val('Input your e-mail address here...');
-		$('.email').focus(function() {
-			if($(this).val() == 'Input your e-mail address here...') {
-				$(this).val('');
-			}
-		});
-	}
-
 	// for detecting if the browser is Safari
 	var browser = navigator.userAgent.toLowerCase();
-
-	if(!Modernizr.input.required || (browser.indexOf("safari") != -1 && browser.indexOf("chrome") == -1)) {
-		$('form').submit(function() {
-			$('.popup').remove();
-			if(!$('.email').val() || $('.email').val() == 'Input your e-mail address here...') {
-				$('form').append('<p class="popup">Please fill out this field.</p>');
-				$('.email').focus();
-				return false;
-			}
-		});
-		$('.email').keydown(function() {
-			$('.popup').remove();
-		});
-		$('.email').blur(function() {
-			$('.popup').remove();
-		});
-	}
-
 
 });
